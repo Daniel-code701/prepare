7a8f4178e5a9d1e5207523ac9c60dcbe57df9a1b
Daniel <feifei282922334@gmail.com>
2020-04-01 22:49:07 +0800

    启动HTTP服务 自动加载配置

---
+++ cron/master/Config.go
@@ -0,0 +1,41 @@
+package master
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+//程序配置
+type Config struct {
+	ApiPort         int `json:"api_port"`
+	ApiReadTimeout  int `json:"api_read_timeout"`
+	ApiWriteTimeout int `json:"api_write_timeout"`
+}
+
+//配置单例
+var (
+	//单例模式
+	G_config *Config
+)
+
+//加载配置
+func InitConfig(filename string) (err error) {
+	var (
+		content []byte
+		conf    Config
+	)
+	//1.把配置文件读出来
+	if content, err = ioutil.ReadFile(filename); err != nil {
+		return
+	}
+
+	//2.json反序列化
+	if err = json.Unmarshal(content, &conf); err != nil {
+		return
+	}
+
+	//3.赋值单例
+	G_config = &conf
+
+	return
+}
---
+++ cron/master/main/master.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"prepare/cron/master"
+	"runtime"
+)
+
+var (
+	confFile string //配置文件路径
+)
+
+//解析命令行参数
+func initArgs() {
+	//master-config ./master.json
+	flag.StringVar(&confFile, "config", "./master.json", "指定master.json")
+	flag.Parse()
+}
+
+//让线程数和CPU核心数量相等
+func initEnv() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+
+	var (
+		err error
+	)
+	//初始化命令行参数
+	initArgs()
+
+	//初始化线程
+	initEnv()
+
+	//加载配置
+	if err = master.InitConfig(confFile); err != nil {
+		goto ERR
+	}
+
+	// 启动Api HTTP服务
+	if err = master.InitApiServer(); err != nil {
+		goto ERR
+	}
+
+	//正常退出
+	return
+
+	//错误打印到屏幕上 如果启动失败 可以看到错误原因
+ERR:
+	fmt.Println(err)
+}
